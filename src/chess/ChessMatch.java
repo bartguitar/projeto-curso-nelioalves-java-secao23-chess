05dd5c40d3234042a852d770052cabbd0a49f70f
bartguitar <adrielbart@gmail.com>
2026-03-18 17:28:34 -0300

    Handling captured pieces

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -6,11 +6,18 @@ import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChessMatch {
 
     private int turn;
     private Color currentPlayer;
     private Board board;
+
+    private List<Piece> piecesOnTheBoard = new ArrayList<>();
+    private List<Piece> capturedPieces = new ArrayList<>();
+
 
 
     public ChessMatch() {
@@ -59,6 +66,12 @@ public class ChessMatch {
         Piece p = board.removePiece(source);
         Piece capturePiece = board.removePiece(target);
         board.placePiece(p, target);
+
+        if(capturePiece != null){
+            piecesOnTheBoard.remove(capturePiece);
+            capturedPieces.add(capturePiece);
+        }
+
         return capturePiece;
     }
 
@@ -89,6 +102,7 @@ public class ChessMatch {
 
     private void placeNewPiece(char column, int row, ChessPiece piece){
         board.placePiece(piece, new ChessPosition(column, row).toPosition());
+        piecesOnTheBoard.add(piece);
     }
 
     private void initialSetup(){
